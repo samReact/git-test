f8df22065001652cff2ce02ed1ca6158ce2b118a
samReact <sam.react001@gmail.com>
2023-04-12 14:15:25 +0200

    :tada: initialize project

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
-import React from 'react';
-import { Button } from '@progress/kendo-react-buttons';
-import kendoka from './kendoka.svg';
-import './App.scss';
+import { FC } from 'react';
 
-function App() {
-  const handleClick = React.useCallback(() => {
-    window.open('https://www.telerik.com/kendo-react-ui/components/', '_blank');
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={kendoka} className="App-logo" alt="kendoka" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button
-          themeColor={'primary'}
-          size={"large"}
-          onClick={handleClick}
-        >
-          Learn KendoReact
-        </Button>
-      </header>
-    </div>
-  );
-}
+const App: FC = (): JSX.Element => {
+  return <div>Hello World !</div>;
+};
 
 export default App;
